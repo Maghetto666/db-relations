4af12ae28fe846a9500d2ecc6440778ed2c60e42
Maghetto666 <lordrey3@gmail.com>
2024-07-25 15:38:26 +0200

    creati controller

--- src/main/java/it/gp/db_relations/repository/UserDetailsRepository.java
+++ src/main/java/it/gp/db_relations/repository/UserDetailsRepository.java
@@ -1,7 +1,8 @@
 package it.gp.db_relations.repository;
 
 import it.gp.db_relations.model.User;
+import it.gp.db_relations.model.UserDetails;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository  extends JpaRepository<User, Long> {
+public interface UserDetailsRepository  extends JpaRepository<UserDetails, Long> {
 }
